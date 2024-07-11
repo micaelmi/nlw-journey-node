6df0ed85247c156aa7d1bf7ca55d958b186a398d
Micael Miranda <micaelmiranda124@gmail.com>
2024-07-11 13:09:56 -0300

    aula 1 parte 1 - setup e criação de viagem

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,71 @@
+import type { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import dayjs from "dayjs";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(3),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+        }),
+      },
+    },
+    async (request) => {
+      const { destination, starts_at, ends_at, owner_name, owner_email } =
+        request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            create: {
+              name: owner_name,
+              email: owner_email,
+              is_owner: true,
+              is_confirmed: true,
+            },
+          },
+        },
+      });
+
+      const mail = await getMailClient();
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Equipe plann.er",
+          address: "oi@plann.er",
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: "Confirmação de criação de viagem",
+        html: `<p>Teste do envio de email</p>`,
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { tripId: trip.id };
+    }
+  );
+}
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createTrip } from "./routes/create-trip";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server flying! 🚀");
+});
